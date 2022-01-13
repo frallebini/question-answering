49eb615ce10d2893ba36de0fbaedb11bb32e04ca
Francesco Ballerini <francesco.ballerini3@studio.unibo.it>
2022-01-13 19:52:59 +0100

    Add SquadV1Example class

--- squad.py
+++ squad.py
@@ -1,26 +1,24 @@
+from __future__ import annotations
 import json
 from collections import OrderedDict
+from tqdm import tqdm
+from transformers.data.processors.squad import SquadExample
 
 
-def rearrange_squad(fname: str) -> None:
+def get_examples_from_dataset(fname: str, fwrite=False) -> list[SquadExample]:
     """
-    Reads the training set of SQuaD 1.1 and creates a .json file
-    storing one sample for each question, where each sample has shape
-    ```
-    id (int): {
-        title (str): ...
-        context (str): ...
-        question (str): ...
-        answer_start (int): ...
-        answer_text (str): ...
-    }
-    ```
+    Reads the training set of SQuaD 1.1 and creates a `SquadExample` object for each question.
+
+    Args:
+        fname: name of the file containing the training set.
+        fwrite: whether to store the examples in a .json file (for visual inspection).
     """
     with open(fname) as f:
         squad = json.load(f)
     
-    samples = OrderedDict()
-    for topic in squad['data']:
+    examples = []
+    fcontent = OrderedDict()
+    for topic in tqdm(squad['data']):
         title = topic['title']
         for paragraph in topic['paragraphs']:
             context = paragraph['context']
@@ -31,18 +29,35 @@ def rearrange_squad(fname: str) -> None:
                 assert len(answers) == 1  # check that no question in the training set has more than one answer
                 answer_start = answers[0]['answer_start']
                 answer_text = answers[0]['text']
-                samples[id] = {
-                    'title': title,
-                    'context': context,
-                    'question': question,
-                    'answer_start': answer_start,
-                    'answer_text': answer_text
-                }
+                
+                example = SquadV1Example(
+                    qas_id=id,
+                    question_text=question,
+                    context_text=context,
+                    answer_text=answer_text,
+                    start_position_character=answer_start,
+                    title=title)
+                examples.append(example)
+                if fwrite: fcontent[example.qas_id] = example.as_dict()
     
-    name, ext = fname.split('.')
-    with open(f'{name}_rearranged.{ext}', 'w') as f:
-        json.dump(samples, f, indent=4)
+    if fwrite:
+        name, ext = fname.split('.')
+        with open(f'{name}_rearranged.{ext}', 'w') as f:
+            json.dump(fcontent, f, indent=4)
 
+    return examples
+
+
+class SquadV1Example(SquadExample):
+    def as_dict(self) -> dict:
+        return {
+            'title': self.title,
+            'context': self.context_text,
+            'question': self.question_text,
+            'answer_start': self.start_position,
+            'answer_text': self.answer_text
+        }
+            
 
 if __name__ == '__main__':
-    rearrange_squad('training_set.json')
+    examples = get_examples_from_dataset('training_set.json')
