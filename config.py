8fbb93443c8165733a74955effb40c119ff336ff
EmmanueleBollino <emmanuele.bollino@studio.unibo.it>
2022-01-22 13:05:37 +0100

    Dataset class

---
+++ config.py
@@ -0,0 +1,7 @@
+# *** temporary solution *** TODO: remove this config and use something like a yaml
+conf = {
+    'DATASET_FILE': 'training_set.json',
+    'TRAIN_RATIO': 0.75,
+    'BATCH_SIZE': 32,
+    'N_EPOCHS': 3
+}
