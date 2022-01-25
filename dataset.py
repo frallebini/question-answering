Fix computation of metrics during training and progress bar

dataset.py
@@ -8,9 +8,8 @@ import math
 
 class SquadDataset(torch.utils.data.Dataset):
     """
-    Represents an instance of the SQuAD 1.1 Dataset
+    PyTorch wrapper for SQuaD 1.1.
     """
-
     def __init__(self,
                  data: pd.DataFrame,
                  tokenizer: transformers.BertTokenizer):
@@ -35,8 +34,8 @@ class SquadDataset(torch.utils.data.Dataset):
     @classmethod
     def _data_from_json(cls, path: str) -> pd.DataFrame:
         """
-        Reads the SQuaD 1.1 training set (a .json file) and stores each question and
-        related information into a Pandas dataframe.
+        Reads the SQuaD 1.1 training set (a JSON file) and stores each question 
+        and related information into a Pandas dataframe.
         """
         with open(path, 'rb') as f:
             squad = json.load(f)
@@ -50,8 +49,8 @@ class SquadDataset(torch.utils.data.Dataset):
                 for question_data in paragraph['qas']:
                     question = question_data['question']
                     id = question_data['id']
-                    # check that no question in the training set has more than one
-                    # answer
+                    # check that no question in the training set has more than 
+                    # one answer
                     assert len(question_data['answers']) == 1
                     answer = question_data['answers'][0]
                     answer_text = answer['text']
@@ -79,23 +78,28 @@ class SquadDataset(torch.utils.data.Dataset):
         return data
 
     @classmethod
-    def from_json(cls, path: str, tokenizer: transformers.BertTokenizer) -> SquadDataset:
+    def from_json(cls, 
+                  path: str, 
+                  tokenizer: transformers.BertTokenizer) -> SquadDataset:
         """
-        Instantiates a SquadDataset from a JSON file
+        Instantiates a `SquadDataset` object from a JSON file.
         """
         return cls(cls._data_from_json(path), tokenizer)
 
     @classmethod
-    def _train_val_split_data(cls, data: pd.DataFrame, train_ratio=0.75) -> tuple[pd.DataFrame, pd.DataFrame]:
+    def _train_val_split_data(cls, 
+                              data: pd.DataFrame, 
+                              train_ratio=0.75
+                              ) -> tuple[pd.DataFrame, pd.DataFrame]:
         """
         Splits the given dataframe into training and validation data.
         Specifically, the first `ceil(n_samples * train_ratio)` samples are
         reserved for training and the remainder for validation.
 
         If, by doing so, the split occurs between samples with the same `title`,
-        then the smaller subset of such samples is moved from training to validation
-        data or viceversa. As a result, training and validation data do not share
-        any `title`.
+        then the smaller subset of such samples is moved from training to 
+        validation data or viceversa. As a result, training and validation data 
+        do not share any `title`.
         """
         n_samples = data.shape[0]
         n_train = math.ceil(n_samples * train_ratio)
@@ -109,8 +113,8 @@ class SquadDataset(torch.utils.data.Dataset):
         # sets
         shared_title = val_data.iloc[0]['title']
         if shared_title not in train_data['title']:
-            # we got lucky: the split occurs precisely where a title ends and the
-            # new one begins
+            # we got lucky: the split occurs precisely where a title ends and 
+            # the new one begins
             return train_data, val_data
 
         shared_title_mask = lambda df: df['title'] == shared_title
@@ -119,8 +123,8 @@ class SquadDataset(torch.utils.data.Dataset):
         n_shared_title_train = shared_title_count(train_data)
         n_shared_title_var = shared_title_count(val_data)
 
-        # if the training set contains less samples with the shared title then the
-        # validation set, then move those samples to the validation set
+        # if the training set contains less samples with the shared title then 
+        # the validation set, then move those samples to the validation set
         if n_shared_title_train < n_shared_title_var:
             train_portion = train_data[shared_title_mask(train_data)]
             val_data = pd.concat((train_portion, val_data), ignore_index=True)
@@ -138,36 +142,45 @@ class SquadDataset(torch.utils.data.Dataset):
 
         return train_data, val_data
 
-    def train_val_split(self, train_ratio=0.75) -> tuple[SquadDataset, SquadDataset]:
+    def train_val_split(self, 
+                        train_ratio=0.75) -> tuple[SquadDataset, SquadDataset]:
         """
-        Splits the current SquadDataset into two SquadDatasets.
-        One for training and the other for validation purposes.
+        Splits the current dataset into two `SquadDataset` objects, one for 
+        training and the other for validation purposes.
         """
         train_data, val_data = self._train_val_split_data(self.data, train_ratio)
-        return SquadDataset(train_data, self.tokenizer), SquadDataset(val_data, self.tokenizer)
+        return (SquadDataset(train_data, self.tokenizer), 
+                SquadDataset(val_data, self.tokenizer))
 
     def update_encoding(self, force=False):
         """
-        Updates the encodings if necessary. Encodings are produced in a lazy way.
+        Updates the encodings if necessary.
+        Encodings are produced in a lazy way.
         """
         if self.encodings is None or self.updated or force:
             self.encodings = self._encode(self.data, self.tokenizer)
 
     @classmethod
-    def _encode(cls, data: pd.DataFrame, tokenizer: transformers.BertTokenizer) -> transformers.BatchEncoding:
+    def _encode(cls, 
+                data: pd.DataFrame, 
+                tokenizer: transformers.BertTokenizer
+                ) -> transformers.BatchEncoding:
         """
-        Creates BERT context-question encodings, i.e. context token indices (a.k.a.
-        input IDs) + [SEP] + question token indices.
+        Creates BERT context-question encodings, i.e. context token indices 
+        (a.k.a. input IDs) + [SEP] + question token indices.
 
-        Encodings will contain three more fields to the `BatchEncoding` object returned by `encode` (which
-        is basically a standard Python dictionary):
+        Three fields will be added to the `BatchEncoding` object returned by 
+        `tokenizer.__call__` (which is basically a standard Python dictionary):
         - The index of the first token of the answer.
         - The index of the last token of the answer.
         - The ID of the answer.
         """
         contexts = list(data['context'])
         questions = list(data['question'])
-        encodings = tokenizer(contexts, questions, padding=True, truncation=True)
+        encodings = tokenizer(contexts, 
+                              questions, 
+                              padding=True, 
+                              truncation=True)
 
         start_positions = []
         end_positions = []
@@ -199,7 +212,7 @@ class SquadDataset(torch.utils.data.Dataset):
 
     def set_data(self, data: pd.DataFrame):
         """
-        Changes the current dataframe of the SquadDataset object.
+        Changes the current dataframe of the `SquadDataset` object.
         """
         self.data = data
         self.updated = True